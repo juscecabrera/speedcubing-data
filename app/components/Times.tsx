5f7063012cc040532a52dbc2a1e8bbfd673a0097
JusceCabrera <jusce240@gmail.com>
2024-11-27 19:20:36 -0500

    add roadmap

--- app/components/Times.tsx
+++ app/components/Times.tsx
@@ -6,12 +6,14 @@ import { useTimer } from '../contexts/TimerContext';
 const Times = () => {
   const { isRunning, sessionData } = useTimer()
 
+  const lastFiveEntries = Object.entries(sessionData).slice(-5).reverse();
+
   return (
     <div className='absolute bottom-0 w-full flex justify-center'>
       {isRunning ? <></>
       :
       <div className='flex flex-row'>
-        {Object.entries(sessionData).map(([key, value]) => (
+        {lastFiveEntries.map(([key, value]) => (
           <TimeCard key={key} id={key} data={value}/>
         ))}
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,23 @@ import Times from "./components/Times";
 import StatsSM from "./components/StatsSM";
 import { TimerProvider } from "./contexts/TimerContext";
 
+/*
+Por hacer:
+1. Hacer la logica para tener los tiempos en localStorage
+2. Poder registrar realmente las soluciones con data precisa
+2. Hacer las animaciones para que cuando se agregue un nuevo tiempo se cree una nueva carta y se elimine la anterior
+3. Hacer la logica de las stats
+4. Poder exportar e importar los tiempos
+5. Poder tener multiples sesiones
+6. Poder modificar soluciones (DNF, +2, Copiar, eliminar, etc)
+7. Arreglar las posiciones de los botones
+8. Agregar tiempo de inspeccion
+9. Agregar autenticacion
+
+
+*/
+
+
 export default function Home() {
   return (
     <div className="bg-speedblue text-white font-jetbrains">
